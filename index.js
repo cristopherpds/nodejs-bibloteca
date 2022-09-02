645ae88712aa42bec9a50c2e1969ad704d9036fc
Cristopher Paiva <josuepaiva7@gmail.com>
2022-09-02 04:46:40 -0300

    Trabalhando com promessas e async/await

--- index.js
+++ index.js
@@ -5,13 +5,29 @@ const trataErro=(erro)=>{
   throw new Error(chalk.red(erro.code, 'caminho do arquivo incorreto'));
 }
 
-const  pegaArquivo = (caminhoDoArquivo) =>{
+const  pegaArquivo = async(caminhoDoArquivo) =>{
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, 'utf-8')
+    console.log(chalk.green(texto));
+  } catch (erro) {
+    trataErro(erro);
+  }
+}
+
+/* const pegaArquivo =(caminhoDoArquivo)=>{
+  fs.promises
+  .readFile(caminhoDoArquivo, 'utf-8')
+  .then((texto)=> console.log(texto))
+  .catch((erro) => trataErro(erro))
+} */
+
+/* const  pegaArquivo = (caminhoDoArquivo) =>{
   fs.readFile(caminhoDoArquivo, 'utf-8', (erro, texto)=>{
     if(erro){
       trataErro(erro);
     }
     console.log(chalk.green(texto));
   })
-}
+} */
 
 pegaArquivo('./arquivos/texto1.md');
